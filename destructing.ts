86f4d824102486b7705d5485d059c7bb95fa78aa
Wesley Xie <WesleyXie0424@gmail.com>
2017-06-23 13:53:15 +0800

    Tweek the destruct example

--- destructing.ts
+++ destructing.ts
@@ -40,20 +40,31 @@ function getAllBooks(): Book[] {
 }
 
 
-function PrintBookInfo({ title, author }: Book): void {
+function printBookInfo({ title, author }: Book): void {
   console.log(`${title} was authored by ${author}`);
 }
 
+// let printBookInfo = ({ title, author }: Book): void => {
+//   console.log(`${title} was authored by ${author}`);
+// };
+
 let [book1, book2] = getAllBooks();
 
-PrintBookInfo(book1);
-PrintBookInfo(book2);
+printBookInfo(book1);
+printBookInfo(book2);
 
 let { title, author } = book1;
 console.log(title);
 console.log(author);
 
-PrintBookInfo(book1);
+printBookInfo(book1);
+
+function printBook1(): void {
+  let {title, author} = this.book1;
+  console.log(`${title} was authored by ${author}`);
+}
+
+printBook1();
 
 let schoolBooks: Book[] = [
   {
@@ -82,4 +93,3 @@ let schoolBooks: Book[] = [
 let booksRead: Book[] = getAllBooks();
 booksRead.push(...schoolBooks);
 console.log(booksRead);
-
